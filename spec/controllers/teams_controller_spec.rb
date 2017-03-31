de1359204b6809800101cb5e28370c1983afe73a
eyingling02 <eyingling02@gmail.com>
2017-03-30 23:00:51 -0700

    Add first test, need to use Factory Girl Gem

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe TeamsController, type: :controller do
-
+  let!(:league) { League.create!(sport: "Soccer") }
+  let!(:team) { Team.create!(name: "TeamErica", league_id: 4) }
+  let!(:game) {  Game.create(address: "123 tree ln", time: Time.now, date: Date.new, home_score: 2, away_score: 4)
+}
 end
